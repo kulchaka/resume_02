9ba5258a7ce43efbc523380c8caf0b8adce5654b
akulchyt <andrii.kulchytskyi@xorux.com>
2025-01-14 18:06:32 +0100

    Update site URL to resume_02

--- astro.config.ts
+++ astro.config.ts
@@ -5,7 +5,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kulchaka.github.io/resume/',
+  site: 'https://kulchaka.github.io/resume_02/',
   integrations: [
     react(),
     tailwind({
