b100fec1ce789962607f6eb58a8516ab9653a88e
akulchyt <andrii.kulchytskyi@xorux.com>
2025-01-14 17:58:43 +0100

    add untracked files

---
+++ src/content/profileData.ts
@@ -0,0 +1,87 @@
+import type { Linkedin } from "lucide-react";
+
+export const PROFILE = {
+  site: {
+    SEO: {
+      title: "Andrii Kulchytskyi - Frontend Developer | Learn, Create, and Innovate ❤️",
+      description:
+        "Frontend Developer passionate about React, TypeScript, Ant Design, and building efficient web applications. Exploring AI, enhancing user experiences, and constantly growing. | Andrii's Astro Portfolio",
+    },
+  },
+  timezone: "Europe/Prague",
+  language: "cs-CZ",
+  firstName: "Andrii",
+  name: "Andrii Kulchytskyi",
+  headLine: "Learn, Share and Grow ⚛️",
+  headLine2:
+    "I enjoy providing software solutions, bringing ideas to life, and solving real-world problems.I take pride in delivering high-quality results and ensuring that every task is completed to the end.",
+  website: "https://techtim42.com",
+  repo: "https://github.com/kulchaka",
+  avatarUrl: "https://kulchaka.github.io/resume/img/pic/clipart2649902213.png",
+  shortIntros: [
+
+    "💻 Frontend Developer | Enthusiast of React JS, TypeScript, Ant Design, and various npm libraries",
+    "☁️ Passionate about creating seamless user experiences and integrating REST API for efficient solutions",
+    "🤖 AI Enthusiast | Fascinated by Apple technology and the intersection of innovation and design",
+    "🌱 Always eager to learn and grow in programming",
+    "📚 Lifelong learner exploring cutting-edge technologies",
+    "🎾 Tennis player | 🎿 Skiing enthusiast | 🌍 Travel lover",
+  ],
+  words: ["Strive for progress, not perfection, and finish what you start."],
+  skills: [
+    "JavaScript",
+    "TypeScript",
+    "React / Ant Design",
+    "Node.js",
+    "Astro",
+    "REST API",
+    "Redux Toolkit ",
+    "Zustand",
+    "HTML / CSS / SCSS",
+    "npm / Vite.js",
+    "PostgreSQL",
+    "SQL",
+    "AI Enthusiast (Exploring AI Tools)",
+  ],
+  links: {
+    github: "https://github.com/kulchaka",
+    linkedin: "https://www.linkedin.com/in/andrii-kul/",
+    cv: "https://kulchaka.github.io/resume/docs/resume.pdf",
+    twitter: "https://twitter.com/TechTim42",
+    bluesky: "https://bsky.app/profile/techtim.bsky.social",
+    thread: "https://www.threads.net/@techtim2024",
+    mastodon: "https://mastodon.social/@techtim42",
+    youtube: "https://www.youtube.com/@techtim42",
+    stackoverflow: "https://stackoverflow.com/users/5249708/tim",
+    devto: "https://dev.to/timhub",
+    goodreads: "",
+  },
+  studies: [
+    {
+      title: "React and TypeScript - Building Modern Web Applications",
+      institution: "Frontend Masters",
+    },
+    {
+      title: "Advanced React Patterns",
+      institution: "Frontend Masters",
+    },
+    {
+      title: "NestJS - Building Scalable Backend Applications",
+      institution: "Udemy",
+    },
+    {
+      title: "Progressive Web Apps (PWA): Build Modern Web Apps",
+      institution: "Coursera",
+    },
+    {
+      title: "State Management with Zustand",
+      platform: "YouTube",
+      creator: "Tech YouTubers",
+    },
+    {
+      title: "AI in Web Development",
+      platform: "YouTube",
+      creator: "OpenAI Tutorials",
+    },
+  ],
+};
