0e60bb01f9a9a7f79623352bf114185b02dbccb9
akulchyt <andrii.kulchytskyi@xorux.com>
2025-01-14 18:07:37 +0100

    Update deployment configuration

--- astro.config.ts
+++ astro.config.ts
@@ -5,7 +5,8 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kulchaka.github.io/resume_02/',
+  site: 'https://kulchaka.github.io',
+  base: '/resume_02',
   integrations: [
     react(),
     tailwind({
